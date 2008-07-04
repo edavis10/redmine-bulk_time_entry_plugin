5fd0268eefe0bdf3f9b466cb946bf978c2572cc5
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-04 11:24:47 -0700

    Time entries can now be saved.
    * Redid the partials and forms to render multiple time entries correctly
    * Added #save method to save multiple entries

--- app/controllers/bulk_time_entries_controller.rb
+++ app/controllers/bulk_time_entries_controller.rb
@@ -6,11 +6,22 @@ class BulkTimeEntriesController < ApplicationController
     # TODO: Get list of projects user has permission
     # TODO: Check projects isn't empty
     @activities = Enumeration::get_values('ACTI')
-    @time_entries = []
+    @projects = Project.find(:all) # TODO: Filter
+    @time_entries = [TimeEntry.new, TimeEntry.new]
   end
   
   def save
-    flash[:notice] = 'Redirected'
+    if request.post? 
+      @time_entries = params[:time_entries]
+      @time_entries.each do |entry|
+        @time_entry = TimeEntry.new(entry)
+        @time_entry.project_id = entry[:project_id] # project_id is protected from mass assignment
+        @time_entry.user = User.current
+        # TODO: Display saved state in flash like bulk issue edit
+        @time_entry.save
+      end
+      flash[:notice] = l(:notice_successful_update)
+    end    
     redirect_to :action => 'index'
   end
 end
