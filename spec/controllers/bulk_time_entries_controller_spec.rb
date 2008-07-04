cb35096095225b2ea9274524b029937ba3a4bc5f
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-04 10:19:48 -0700

    Added RSpec spec files

---
+++ spec/controllers/bulk_time_entries_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe BulkTimeEntriesController do
+
+  #Delete this example and add some real ones
+  it "should use BulkTimeEntriesController" do
+    controller.should be_an_instance_of(BulkTimeEntriesController)
+  end
+
+end
