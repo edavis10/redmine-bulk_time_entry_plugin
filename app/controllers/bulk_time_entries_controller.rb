78ae78aeb7fa328f08819aac0f1c5a263123adb0
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-04 10:40:14 -0700

    Hacking through the multiple forms

--- app/controllers/bulk_time_entries_controller.rb
+++ app/controllers/bulk_time_entries_controller.rb
@@ -3,5 +3,14 @@ class BulkTimeEntriesController < ApplicationController
   layout 'base'
   
   def index
+    # TODO: Get list of projects user has permission
+    # TODO: Check projects isn't empty
+    @activities = Enumeration::get_values('ACTI')
+    @time_entries = []
+  end
+  
+  def save
+    flash[:notice] = 'Redirected'
+    redirect_to :action => 'index'
   end
 end
