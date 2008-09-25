6301d2b8b0b3306d2c69af576d6f658363cc4b37
Eric Davis <edavis@littlestreamsoftware.com>
2008-09-25 16:06:01 -0700

    Corrected the plugin title to support localization. #1629

--- init.rb
+++ init.rb
@@ -3,11 +3,11 @@ require 'redmine'
 
 RAILS_DEFAULT_LOGGER.info 'Starting Bulk Time Entry Plugin for RedMine'
 
-Redmine::Plugin.register :bulk_time_entry do
+Redmine::Plugin.register :bulk_time_entry_plugin do
   name 'Bulk Time Entry'
   author 'Eric Davis'
   description 'This is a plugin to help enter multiple timelogs at one time'
   version '0.0.0'
   
-  menu :top_menu, :bulk_time_entries, :controller => "bulk_time_entries", :action => 'index'
+  menu :top_menu, :bulk_time_entry, { :controller => "bulk_time_entries", :action => 'index' }, :caption => :bulk_time_entry_title
 end
