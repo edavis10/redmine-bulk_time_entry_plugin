ff32c306e7a14e8174071ada9af9b217f2143943
Michele Franzin <michele.franzin@gmail.com>
2008-09-30 17:01:09 +0200

    hide menu link for non-logged users

--- init.rb
+++ init.rb
@@ -9,5 +9,6 @@ Redmine::Plugin.register :bulk_time_entry_plugin do
   description 'This is a plugin to help enter multiple timelogs at one time'
   version '0.0.0'
   
-  menu :top_menu, :bulk_time_entry, { :controller => "bulk_time_entries", :action => 'index' }, :caption => :bulk_time_entry_title
+  menu :top_menu, :bulk_time_entry, {:controller => "bulk_time_entries", :action => 'index'}, 
+    :caption => :bulk_time_entry_title, :if => Proc.new{User.current.logged?} 
 end
