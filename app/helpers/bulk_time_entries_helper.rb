1e7248192d604a54ce9ed28fef3852ac3b231607
Michele Franzin <michele.franzin@gmail.com>
2008-11-09 15:09:16 +0100

    BUGFIX: label of fields pointed to a wrong id

--- app/helpers/bulk_time_entries_helper.rb
+++ app/helpers/bulk_time_entries_helper.rb
@@ -1,12 +1,12 @@
 module BulkTimeEntriesHelper
   # Cheap knock off of the tabular form builder's labeling
-  def label_for_field(field, options = { })
+  def label_for_field(field, rnd_id, options = { })
     label_text = l(options[:label]) if options[:label]
     label_text ||= l(("field_"+field.to_s.gsub(/\_id$/, "")).to_sym)
     label_text << @template.content_tag("span", " *", :class => "required") if options.delete(:required)
     label = @template.content_tag("label", label_text, 
-      :class => (@object && @object.errors[field] ? "error" : nil), 
-      :for => (@object_name.to_s + "_" + field.to_s))
+      :class => (@object && @object.errors.on(field) ? "error" : nil), 
+      :for => (@object_name.to_s + "_" + rnd_id.to_s + "_" + field.to_s))
     label
   end
   
