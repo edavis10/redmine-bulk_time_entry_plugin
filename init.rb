b4e1a576eae2e7c46ba1724c73ef20b1c189db60
Eric Davis <edavis@littlestreamsoftware.com>
2008-11-10 11:09:25 -0800

    Increemnt version to 0.1.0

--- init.rb
+++ init.rb
@@ -1,13 +1,10 @@
-# Empty redmine plguin
 require 'redmine'
-
-RAILS_DEFAULT_LOGGER.info 'Starting Bulk Time Entry Plugin for RedMine'
 
 Redmine::Plugin.register :bulk_time_entry_plugin do
   name 'Bulk Time Entry'
   author 'Eric Davis'
-  description 'This is a plugin to help enter multiple timelogs at one time'
-  version '0.0.0'
+  description 'This is a plugin to enter multiple tiem entries at one time.'
+  version '0.1.0'
   
   menu :top_menu, :bulk_time_entry, {:controller => "bulk_time_entries", :action => 'index'}, 
     :caption => :bulk_time_entry_title, :if => Proc.new{User.current.logged?} 
