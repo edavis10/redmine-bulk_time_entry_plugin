51299f2fff44d9588addc264eb08c2661b3e47b8
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-18 20:45:44 -0700

    Bumping version to 0.3.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :bulk_time_entry_plugin do
   name 'Bulk Time Entry'
   author 'Eric Davis'
   description 'This is a plugin to enter multiple time entries at one time.'
-  version '0.2.0'
+  version '0.3.0'
   
   menu :top_menu, :bulk_time_entry, {:controller => "bulk_time_entries", :action => 'index'}, 
     :caption => :bulk_time_entry_title, :if => Proc.new{User.current.allowed_to?(:log_time, nil, :global => true)} 
