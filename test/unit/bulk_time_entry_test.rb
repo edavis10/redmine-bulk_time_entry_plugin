8cc2fddf7f67ca66411d2f8db1348e04308c40e2
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-28 16:17:47 -0700

    Extracted data generation test method.

--- test/unit/bulk_time_entry_test.rb
+++ test/unit/bulk_time_entry_test.rb
@@ -10,6 +10,19 @@ class BulkTimeEntryTest < Test::Unit::TestCase
     file
   end
 
+  def generate_csv_data(count=5)
+    @project ||= Project.generate!
+    @tracker ||= Tracker.generate!
+    @project.trackers << @tracker unless @project.trackers.include? @tracker
+
+    csv_data = []
+    5.times {
+      issue = Issue.generate!(:tracker => @tracker, :project => @project)
+      csv_data << [issue.id.to_s, 'A comment', Date.today.to_s, @activity.name, (rand * 10).round(2).to_s, @user.login]
+          }
+    csv_data
+  end
+
   context "#import_from_csv" do
 
     context "non-readable file" do
@@ -54,16 +67,7 @@ class BulkTimeEntryTest < Test::Unit::TestCase
       
       context "with valid imports" do
         setup do
-          @project = Project.generate!
-          @tracker = Tracker.generate!
-          @project.trackers << @tracker
-
-          @csv_data = []
-          5.times {
-            issue = Issue.generate!(:tracker => @tracker, :project => @project)
-            @csv_data << [issue.id.to_s, 'A comment', Date.today.to_s, @activity.name, (rand * 10).round(2).to_s, @user.login]
-          }
-
+          @csv_data = generate_csv_data
           @file = mock_csv_file('/csv/valid.csv',
                                 @csv_data.collect {|row| row.join(', ')}.join("\n"))
         end
