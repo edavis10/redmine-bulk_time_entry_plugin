d3524aa2e1d58fa7634cdb82d954f345d17424f7
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-01 13:44:30 -0700

    Provide some feedback when a TimeEntry doesn't save.

--- app/models/bulk_time_entry.rb
+++ app/models/bulk_time_entry.rb
@@ -19,20 +19,20 @@ class BulkTimeEntry
             next
 
           end
-          time = TimeEntry.new(:issue_id => row[0],
+          @time = TimeEntry.new(:issue_id => row[0],
                                :spent_on => row[2],
                                :activity => TimeEntryActivity.find_by_name(row[3].strip),
                                :hours => row[4])
           # Truncate comments to 255 chars
-          time.comments = row[1].mb_chars[0..255].strip.to_s if row[1].present?
-          time.user = User.find_by_login(row[5].strip)
+          @time.comments = row[1].mb_chars[0..255].strip.to_s if row[1].present?
+          @time.user = User.find_by_login(row[5].strip)
 
-          time.save!
+          @time.save!
           row_counter += 1
         end
       end
     rescue ActiveRecord::StatementInvalid, ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid => ex
-      return "ERROR: #{ex.message}"
+      return "ERROR: #{ex.message} on:\n\n#{@time.inspect}"
     end
     failed_message = failed_counter == 0 ? '' : "#{failed_counter} records failed to import."
     return "Imported #{row_counter} records. #{failed_message}"
