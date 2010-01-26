c0d10f8d2bf94ee226bc403453616e37e81f7283
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-25 17:06:28 -0800

    [#3505] Removed compatibility hacks for Redmine 0.8

--- lib/bulk_time_entry_compatibility.rb
+++ lib/bulk_time_entry_compatibility.rb
@@ -1,22 +1,3 @@
-begin
-  require 'time_entry_activity'
-rescue LoadError
-  # TimeEntryActivity is not available
-end
-
 # Wrappers around the Redmine core API changes between versions
 module BulkTimeEntryCompatibility
-  class Enumeration
-    # Wrapper around Redmine's API since Enumerations changed in r2472
-    # This can be removed once 0.9.0 is stable
-    def self.activities
-      if Object.const_defined?('TimeEntryActivity')
-        return ::TimeEntryActivity.all
-      elsif ::Enumeration.respond_to?(:activities)
-        return ::Enumeration.activities
-      else
-        return ::Enumeration::get_values('ACTI')
-      end
-    end
-  end
 end
