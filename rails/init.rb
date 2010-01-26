d3f2cb72ebce56f50562277320e70122de5480f1
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-25 17:01:42 -0800

    Easier to have the init.rb hold the code and rails/init to load init.rb

--- rails/init.rb
+++ rails/init.rb
@@ -1,13 +1,1 @@
-require 'redmine'
-
-config.gem 'fastercsv' if respond_to? :config
-
-Redmine::Plugin.register :bulk_time_entry_plugin do
-  name 'Bulk Time Entry'
-  author 'Eric Davis'
-  description 'This is a plugin to enter multiple time entries at one time.'
-  version '0.4.0'
-  
-  menu :top_menu, :bulk_time_entry, {:controller => "bulk_time_entries", :action => 'index'}, 
-    :caption => :bulk_time_entry_title, :if => Proc.new{User.current.allowed_to?(:log_time, nil, :global => true)} 
-end
+require File.dirname(__FILE__) + "/../init"
