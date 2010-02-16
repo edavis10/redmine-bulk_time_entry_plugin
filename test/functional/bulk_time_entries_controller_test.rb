99de479e817a31b3204c1154c0ae10f4bb7f47c0
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-15 17:30:02 -0800

    Added tests for the #save action

--- test/functional/bulk_time_entries_controller_test.rb
+++ test/functional/bulk_time_entries_controller_test.rb
@@ -44,4 +44,95 @@ class BulkTimeEntriesControllerTest < ActionController::TestCase
       should_render_template :index
     end
   end
+
+  context "POST to :save with valid params and a valid user" do
+    setup do
+      @project = Project.generate!
+      @activity = TimeEntryActivity.generate!
+
+      generate_user_and_login_for_project(@project)
+      @issue = Issue.generate!(:project => @project, :tracker => @project.trackers.first, :priority => IssuePriority.generate!(:name => 'Low'))
+    end
+
+    should "save a new time entry" do
+
+      assert_difference('TimeEntry.count',2) do
+        post :save, :time_entries => {
+          "1234" => {
+            "comments" => 'a comment',
+            "project_id" => @project.id,
+            "issue_id" => @issue.id,
+            "activity_id" => @activity.id,
+            "spent_on" => Date.today.to_s,
+            "hours" => '42'
+          },
+          "9658" => {
+            "comments" => 'a comment',
+            "project_id" => @project.id,
+            "issue_id" => @issue.id,
+            "activity_id" => @activity.id,
+            "spent_on" => Date.today.to_s,
+            "hours" => '42'
+          }
+        }
+      end
+      
+    end
+
+    should "replace the time entry form with a flash message" do
+      post :save, :time_entries => {
+        "1234" => {
+          "comments" => 'a comment',
+          "project_id" => @project.id,
+          "issue_id" => @issue.id,
+          "activity_id" => @activity.id,
+          "spent_on" => Date.today.to_s,
+          "hours" => '42'
+        }
+      }
+
+      assert_select_rjs :replace_html, 'entry_1234', :text => /notice/
+    end
+  end
+
+  context "POST to :save with invalid params and a valid user" do
+    setup do
+      @project = Project.generate!
+      @activity = TimeEntryActivity.generate!
+
+      generate_user_and_login_for_project(@project)
+      @issue = Issue.generate!(:project => @project, :tracker => @project.trackers.first, :priority => IssuePriority.generate!(:name => 'Low'))
+    end
+    should "not save a time entry" do
+
+      assert_no_difference('TimeEntry.count') do
+        post :save, :time_entries => {
+          "1234" => {
+            "comments" => 'a comment',
+            "project_id" => @project.id,
+            "issue_id" => @issue.id,
+            "activity_id" => @activity.id,
+            "spent_on" => '',
+            "hours" => '42'
+          }
+        }
+      end
+      
+    end
+
+    should "re-render the time entry from" do
+      post :save, :time_entries => {
+        "1234" => {
+          "comments" => 'a comment',
+          "project_id" => @project.id,
+          "issue_id" => @issue.id,
+          "activity_id" => @activity.id,
+          "spent_on" => '',
+          "hours" => '42'
+        }
+      }
+
+      assert_select_rjs :replace, 'entry_1234', :text => /blank/
+    end
+  end
 end
